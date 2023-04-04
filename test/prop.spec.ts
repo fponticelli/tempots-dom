10495b87713e021b0ad19484c71bed57da56798f
Franco Ponticelli <franco.ponticelli@gmail.com>
2023-04-03 19:55:48 -0600

    Add coverage

---
+++ test/prop.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest'
+import { Prop } from '../src/prop'
+
+describe('Prop', () => {
+  test('get/set', () => {
+    const prop = new Prop(1)
+    expect(prop.get()).toBe(1)
+    prop.set(2)
+    expect(prop.get()).toBe(2)
+  })
+})
