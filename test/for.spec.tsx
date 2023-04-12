16d7fbd3c606e4ce6fef9de86795990e4489260b
Franco Ponticelli <franco.ponticelli@gmail.com>
2023-04-12 23:08:27 +0000

    Add separator for Repeat/For and count/animate for Signal

---
+++ test/for.spec.tsx
@@ -0,0 +1,45 @@
+/**
+ * @jsxImportSource ../src
+ */
+
+import { render, If, Prop, For, Signal } from '../src'
+
+describe('For', () => {
+  afterEach(() => {
+    document.body.innerHTML = ''
+    document.head.innerHTML = ''
+  })
+
+  test('without separator', () => {
+    const prop = Prop.of([1, 2, 3])
+    const view = <For of={prop}>{(v: Signal<number>) => v.map(String)}</For>
+    render(view, document.body)
+    expect(document.body.innerHTML).toBe('123')
+    prop.set([4, 5])
+    expect(document.body.innerHTML).toBe('45')
+    prop.set([])
+    expect(document.body.innerHTML).toBe('')
+    prop.set([9, 8, 7, 6, 5, 4, 3, 2, 1])
+    expect(document.body.innerHTML).toBe('987654321')
+  })
+
+  test('with separator', () => {
+    const prop = Prop.of([1, 2, 3])
+    const view = <For
+      of={prop}
+      separator={s => s.map(v => {
+        return v.last ? '!' : v.first ? ':' : ','
+      })}
+    >{(v: Signal<number>) => v.map(String)}</For>
+    render(view, document.body)
+    expect(document.body.innerHTML).toBe('1:2!3')
+    prop.set([4, 5])
+    expect(document.body.innerHTML).toBe('4!5')
+    prop.set([6])
+    expect(document.body.innerHTML).toBe('6')
+    prop.set([])
+    expect(document.body.innerHTML).toBe('')
+    prop.set([9, 8, 7, 6, 5, 4, 3, 2, 1])
+    expect(document.body.innerHTML).toBe('9:8,7,6,5,4,3,2!1')
+  })
+})
